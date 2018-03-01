176281926492ae15b06571dabb91f087eacd0f58
« SophieTochon <sophie.tochon@gmail.com>
2018-03-01 08:21:29 +0100

    Create Email model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Email.create! object: "First email"
+			  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+Email.create! object: "Second email"
+		 	  body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+
+Email.create! object: "Third email"
+			  body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+
+Email.create! object: "Fourth email"
+			  body: "Pellentesque molestie velit at metus hendrerit dapibus. Aenean id tristique ex, nec consequat sem. Cras suscipit magna at cursus ullamcorper. Suspendisse dictum euismod maximus. Vestibulum pellentesque mattis est, ac molestie risus posuere molestie. Fusce lacinia ligula auctor dolor imperdiet, non maximus eros sollicitudin. Donec eget nisl eget erat varius commodo. Aliquam tincidunt tincidunt feugiat. Ut at dolor fermentum eros tristique venenatis. Nullam a ultrices lorem. Donec tincidunt lectus non blandit elementum. Pellentesque scelerisque non magna vel venenatis. Etiam eget lobortis diam, et sodales mauris. Mauris aliquet semper porta. Nullam iaculis felis ut posuere lobortis. Quisque a turpis in nisl malesuada rutrum quis vel lorem."
---
+++ spec/factories/emails.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :email do
+    object "MyString"
+    body "MyText"
+    opened false
+  end
+end
