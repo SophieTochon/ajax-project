fcf9c6b20d24c0268f9a86006a5d8587081550ce
« SophieTochon <sophie.tochon@gmail.com>
2018-03-01 14:57:48 +0100

    Giving up :(

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -8,6 +8,25 @@ class EmailsController < ApplicationController
   	@email = Email.create!(emails_params)
   end
 
+  def show
+    @email = Email.find.params([:id])
+
+    respond_to do |f|
+      f.html { redirect_to emails_url }
+      f.js
+    end
+  end
+
+  def destroy
+    @email = Email.find.params([:id])
+    @email.delete
+
+    respond_to do |f|
+      f.html { redirect_to emails_url }
+      f.js
+    end
+  end
+
   private
 
   	def emails_params
